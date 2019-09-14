ce638d56ce421f04c71b84d5f1b953387357ae99
Mihir Patel <ee.mihir.patel@gmail.com>
2019-09-14 16:12:42 -0400

    Add TranslateIcon as Favicon/Logo

--- src/AppBar/index.js
+++ src/AppBar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import TranslateIcon from '@material-ui/icons/Translate';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -9,6 +10,9 @@ import GithubIconLink from './GithubIconLink';
 
 const useStyles = makeStyles(theme => ({
   appBarSpacer: theme.mixins.toolbar,
+  logo: {
+    margin: 10,
+  },
   title: {
     flexGrow: 1,
   },
@@ -21,6 +25,7 @@ export default function CustomAppBar() {
     <React.Fragment>
       <AppBar position="absolute">
         <Toolbar>
+          <TranslateIcon className={styles.logo} />
           <Typography variant="h6" className={styles.title}>Elasticsearch Analysis Inspector</Typography>
           <GithubIconLink href="https://github.com/eemp/elasticsearch-analysis-inspector"/>
         </Toolbar>
