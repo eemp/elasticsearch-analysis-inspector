a1358dd8dacf4942645c225700271ef018227578
Mihir Patel <ee.mihir.patel@gmail.com>
2019-09-14 13:10:21 -0400

    Update with custom black/indigo theme

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-//import { makeStyles, withStyles } from '@material-ui/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import Analysis, { EDIT_MODE } from './Analysis';
+import theme from './theme';
 import updateAnalyses from './services/analyze';
 
 import { analyses, sampleText } from './sample-data';
@@ -97,55 +98,57 @@ class App extends React.Component {
     const { analyses, selectedStartOffset, text } = this.state;
 
     return (
-      <div className={styles.app}>
-        <AppBar position="absolute">
-          <Toolbar>
-            <Typography variant="h6">Elasticsearch Analysis Inspector</Typography>
-          </Toolbar>
-        </AppBar>
-        <div className={styles.appBarSpacer} />
-        <Grid container>
-          <Grid item xs={12}>
-            <Container className={styles.section}>
-              <TextField
-                defaultValue={text}
-                fullWidth
-                label="Text"
-                onChange={
-                  ev => {
-                    const text = ev.target.value;
-                    ev.persist();
-                    this.setState({text}, this.onChange);
+      <ThemeProvider theme={theme}>
+        <div className={styles.app}>
+          <AppBar position="absolute">
+            <Toolbar>
+              <Typography variant="h6">Elasticsearch Analysis Inspector</Typography>
+            </Toolbar>
+          </AppBar>
+          <div className={styles.appBarSpacer} />
+          <Grid container>
+            <Grid item xs={12}>
+              <Container className={styles.section}>
+                <TextField
+                  defaultValue={text}
+                  fullWidth
+                  label="Text"
+                  onChange={
+                    ev => {
+                      const text = ev.target.value;
+                      ev.persist();
+                      this.setState({text}, this.onChange);
+                    }
                   }
+                  variant="outlined"
+                />
+              </Container>
+            </Grid>
+            <Grid item xs={12}>
+              <Container>
+                <Button variant="contained" color="secondary" onClick={this.onAdd} style={{float: 'right'}}>New Analyzer</Button>
+              </Container>
+            </Grid>
+            <Grid item xs={12}>
+              <Container>
+                {
+                  _.map(analyses, analysis => (
+                    <Analysis
+                      {...analysis}
+                      className={styles.section}
+                      key={analysis.key}
+                      onClose={this.createCloseFn(analysis.key)}
+                      onTokenSelect={this.onTokenSelect}
+                      selectedStartOffset={selectedStartOffset}
+                      text={text}
+                    />
+                  ))
                 }
-                variant="outlined"
-              />
-            </Container>
+              </Container>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Container>
-              <Button variant="contained" color="secondary" onClick={this.onAdd} style={{float: 'right'}}>New Analyzer</Button>
-            </Container>
-          </Grid>
-          <Grid item xs={12}>
-            <Container>
-              {
-                _.map(analyses, analysis => (
-                  <Analysis
-                    {...analysis}
-                    className={styles.section}
-                    key={analysis.key}
-                    onClose={this.createCloseFn(analysis.key)}
-                    onTokenSelect={this.onTokenSelect}
-                    selectedStartOffset={selectedStartOffset}
-                    text={text}
-                  />
-                ))
-              }
-            </Container>
-          </Grid>
-        </Grid>
-      </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
