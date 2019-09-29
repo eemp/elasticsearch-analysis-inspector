2e6ab008e7163de0d48b09f877012cadd713c2c0
Mihir Patel <ee.mihir.patel@gmail.com>
2019-09-29 01:51:20 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elasticsearch-analysis-inspector/precache-manifest.3a8d40966e9b69286fc6505535421f08.js"
+  "/elasticsearch-analysis-inspector/precache-manifest.6cd33884abe1b70736afd726c721258f.js"
 );
 
 self.addEventListener('message', (event) => {
