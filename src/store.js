ffb51bdb73cfe2dd53656e930f81ef9e1329ae6f
Mihir Patel <ee.mihir.patel@gmail.com>
2019-10-12 00:47:16 -0400

    feat: track app state in url query strings to allow sharing urls

--- src/store.js
+++ src/store.js
@@ -1,20 +1,54 @@
-import { applyMiddleware, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import _ from 'lodash';
+import createBrowserHistory from 'history/createBrowserHistory';
+import jsonpack from 'jsonpack';
+import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createReduxLocationActions, listenForHistoryChange } from 'redux-location-state';
 
-import rootReducer from './reducers'
+import rootReducer from './reducers';
+
+const history = createBrowserHistory();
+
+const paramSetup = { global: {} };
+const mapLocationToState = (state, location) => {
+  const compressedStateFromLocationQuery = _.isString(location.search) && (new URLSearchParams(location.search.substring(1))).get('q');
+  return compressedStateFromLocationQuery
+    ? decompress(compressedStateFromLocationQuery)
+    : state;
+};
+const overwriteLocationHandling = (initialState, nextState, location) => {
+  return {
+    location: _.assign({}, location, {
+      search: `?q=${compress(nextState)}`,
+      shouldPush: true,
+    }),
+  };
+};
+
+const { locationMiddleware, reducersWithLocation } = createReduxLocationActions(paramSetup, mapLocationToState, history, rootReducer, overwriteLocationHandling);
 
 export default function configureStore(preloadedState) {
-  const middlewares = [thunkMiddleware];
+  const middlewares = [thunkMiddleware, locationMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
   const enhancers = [middlewareEnhancer];
   const composedEnhancers = composeWithDevTools(...enhancers);
 
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  const store = createStore(reducersWithLocation, preloadedState, composedEnhancers);
   if(process.env.NODE_ENV !== 'production' && module.hot) {
     module.hot.accept('./reducers', () => store.replaceReducer(rootReducer));
   }
 
+  listenForHistoryChange(store, history);
+
   return store;
+}
+
+function compress(state) {
+  return jsonpack.pack(state);
+}
+
+function decompress(compressedState) {
+  return jsonpack.unpack(compressedState);
 }
